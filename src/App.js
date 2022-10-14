91c9835e2700becf9512fc2831a96917695f965e
KimboSlice <kimberlyhoward529@gmail.com>
2022-10-13 21:43:44 -0400

    Started to create login and sign up page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+// Packages
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './index.css';
+
+// Components
+import LoginSignUp from './components/LoginSignUp';
+import Login from './components/Login';
+import SignUp from './components/SignUp'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+        path="/"
+        element={<LoginSignUp />}/>
+
+        <Route
+        path="/login"
+        element={<Login />}/>
+
+        <Route
+        path="/signup"
+        element={<SignUp />}/>
+      </Routes>
+    </Router>
   );
 }
 
